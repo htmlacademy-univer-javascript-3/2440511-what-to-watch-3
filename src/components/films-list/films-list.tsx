c98f4e3624d078e893b97f3942815721aa58bb62
Ivan Krasheninnikov <krash577388@mail.ru>
2023-11-30 15:48:47 +0500

    made active (Show more) button

--- src/components/films-list/films-list.tsx
+++ src/components/films-list/films-list.tsx
@@ -5,13 +5,17 @@ export interface FilmsListProps {
   filmsData: {filmTitle: string; imgName: string}[];
 }
 
-export function FilmsList({filmsData}: FilmsListProps){
+interface Props {
+  filmsCount: number;
+}
+
+export function FilmsList({filmsData, filmsCount}: FilmsListProps & Props){
   const [, setActiveCard] = useState<string>();
 
   return (
     <div className="catalog__films-list">
       {
-        filmsData.map((x) =>
+        filmsData.slice(0, filmsCount).map((x) =>
           <FilmCard key={x.filmTitle} filmTitle={x.filmTitle} imgName={x.imgName} onMouseEnter={setActiveCard}/>)
       }
     </div>
